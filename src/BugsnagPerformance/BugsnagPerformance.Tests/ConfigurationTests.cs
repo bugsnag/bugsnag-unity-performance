77620576f0e20668126cbf33ab3152e74c7cb34f
Richard Elms <rich@bugsnag.com>
2023-01-19 08:59:29 +0100

    Start simple span

--- src/BugsnagPerformance/BugsnagPerformance.Tests/ConfigurationTests.cs
+++ src/BugsnagPerformance/BugsnagPerformance.Tests/ConfigurationTests.cs
@@ -19,5 +19,26 @@ namespace BugsnagPerformance.Tests
         {
             new PerformanceConfiguration("227df1042bc7772c321dbde3b31a03c2");
         }
+
+        [Test]
+        public void CallStartAfterInit()
+        {
+            var config = new PerformanceConfiguration("227df1042bc7772c321dbde3b31a03c2");
+            BugsnagPerformance.Start(config);
+            Assert.Throws(typeof(Exception), () =>
+            {
+                BugsnagPerformance.Start(config);
+            });
+        }
+
+        [Test]
+        public void CreateSimpleSpan()
+        {
+            var span = BugsnagPerformance.StartSpan("test");
+            Assert.AreEqual("test",span.Name);
+            Assert.AreEqual(SpanKind.SPAN_KIND_INTERNAL,span.Kind);
+            Assert.AreEqual(36,span.TraceId.Length);
+
+        }
     }
 }
--- src/BugsnagPerformance/BugsnagPerformance/BugsnagPerformance.cs
+++ src/BugsnagPerformance/BugsnagPerformance/BugsnagPerformance.cs
@@ -1,4 +1,6 @@
-﻿namespace BugsnagPerformance
+﻿using System;
+
+namespace BugsnagPerformance
 {
     public class BugsnagPerformance
     {
@@ -10,8 +12,23 @@
 
         public static void Start(PerformanceConfiguration performanceConfiguration)
         {
+            if (_isStarted)
+            {
+                // This will be replaced with a Unity warning log once the unity engine dlls are imported
+                throw new Exception("Already started");
+            }
             _configuration = performanceConfiguration;
             _isStarted = true;
+        }
+
+        public static Span StartSpan(string name)
+        {
+            return StartSpan(name, DateTimeOffset.Now);
+        }
+
+        public static Span StartSpan(string name, DateTimeOffset startTime)
+        {
+            return SpanFactory.StartCustomSpan(name, startTime);
         }
     }
 }
