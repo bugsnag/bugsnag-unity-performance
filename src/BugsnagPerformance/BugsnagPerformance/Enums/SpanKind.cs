ca084c92c04521354476558c154060106b233c82
Richard Elms <richardelms@gmail.com>
2023-03-07 14:35:32 +0100

    PLAT-9466 Update enum encoding to use integer values (#6)

--- src/BugsnagPerformance/BugsnagPerformance/Enums/SpanKind.cs
+++ src/BugsnagPerformance/BugsnagPerformance/Enums/SpanKind.cs
@@ -2,10 +2,11 @@
 {
     public enum SpanKind
     {
-        SPAN_KIND_INTERNAL,
-        SPAN_KIND_SERVER,
-        SPAN_KIND_CLIENT,
-        SPAN_KIND_PRODUCER,
-        SPAN_KIND_CONSUMER
+        SPAN_KIND_UNSPECIFIED = 0,
+        SPAN_KIND_INTERNAL = 1,
+        SPAN_KIND_SERVER = 2,
+        SPAN_KIND_CLIENT = 3,
+        SPAN_KIND_PRODUCER = 4,
+        SPAN_KIND_CONSUMER = 5
     }
 }
--- src/BugsnagPerformance/BugsnagPerformance/Models/SpanModel.cs
+++ src/BugsnagPerformance/BugsnagPerformance/Models/SpanModel.cs
@@ -5,7 +5,7 @@ namespace BugsnagUnityPerformance
     internal class SpanModel
     {
         public string name;
-        public string kind;
+        public int kind;
         public string spanId;
         public string traceId;
         public string startTimeUnixNano;
@@ -15,8 +15,8 @@ namespace BugsnagUnityPerformance
         public SpanModel(Span span)
         {
             name = span.Name;
-            kind = span.Kind.ToString();
-            spanId = span.Id.ToString("x");
+            kind = (int)span.Kind;
+            spanId = span.Id;
             traceId = span.TraceId.Replace("-",string.Empty);
             startTimeUnixNano = (span.StartTime.Ticks * 100).ToString();
             endTimeUnixNano = (span.EndTime.Ticks * 100).ToString();
