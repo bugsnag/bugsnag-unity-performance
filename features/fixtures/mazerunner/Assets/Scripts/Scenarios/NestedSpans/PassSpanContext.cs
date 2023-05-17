1e0184e2fd53ef6002bdbcee804c059f0e0aa9b6
Richard Elms <richardelms@gmail.com>
2023-05-17 15:29:57 +0200

    Add span context tests

---
+++ features/fixtures/mazerunner/Assets/Scripts/Scenarios/NestedSpans/PassSpanContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using BugsnagUnityPerformance;
+using UnityEngine;
+
+public class PassSpanContext : Scenario
+{
+    public override void PrepareConfig(string apiKey, string host)
+    {
+        base.PrepareConfig(apiKey, host);
+        SetMaxBatchSize(2);
+    }
+
+    public override void Run()
+    {
+        var span1 = BugsnagPerformance.StartSpan("span1");
+        new Thread(() => {
+            var span2 = BugsnagPerformance.StartSpan("span2",new SpanOptions { ParentContext = span1 });
+            span2.End();
+        }).Start();
+
+        span1.End();
+    }
+}
