3e1a48c71a7134b18bb29417ebbe8c0cb6d9fe37
Richard Elms <richardelms@gmail.com>
2023-05-19 15:22:16 +0200

    initial setup

---
+++ upm/package/README.md
@@ -0,0 +1,17 @@
+Bugsnag Performance SDK for Unity
+===========
+
+This is the UPM package for the Bugsnag Performance SDK for Unity.
+
+Install it via `UPM`:
+```
+TBD
+```
+
+## Source code
+
+The source code for this project is located at: TBD
+
+## Resources
+
+* [Documentation](https://docs.bugsnag.com/platforms/unity/)
---
+++ upm/scripts/build-upm-package.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SRC_DIR=../../BugsnagPerformance/Assets/BugsnagPerformance/
+PKG_DIR=../package
+
+# Check for unity version
+if [ -z "$1" ]
+then
+  echo "ERROR: No Version Set, please pass a version string as the first argument"
+  exit 1
+fi
+
+VERSION=$1
+
+
+echo "Copying over the src"
+
+echo "$PWD"
+
+cp -a  "$SRC_DIR." "$PKG_DIR/Runtime"
+
+
+
+# Set the specified version in the manifest
+
+echo "Setting the version $VERSION in the copied manifest and readme"
+sed -i '' "s/VERSION_STRING/$VERSION/g" "$PKG_DIR/package.json"
+sed -i '' "s/VERSION_STRING/v$VERSION/g" "$PKG_DIR/README.md"
+
+
+echo "complete, ready to deploy"
