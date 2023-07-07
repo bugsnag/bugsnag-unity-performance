a5342ac8a8e1baba5632f0eeb1025e7b428d8e61
Richard Elms <richardelms@gmail.com>
2023-07-07 09:23:51 +0200

    longer timeout to allow first request to process

--- features/fixtures/mazerunner/Assets/Scripts/Scenarios/Persistence/PValueRetry.cs
+++ features/fixtures/mazerunner/Assets/Scripts/Scenarios/Persistence/PValueRetry.cs
@@ -1,14 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
-using BugsnagUnityPerformance;
-using UnityEngine;
-
+﻿
 public class PValueRetry : Scenario
 {
     public override void PrepareConfig(string apiKey, string host)
     {
         base.PrepareConfig(apiKey, host);
-        SetPValueCheckIntervalSeconds(1);
+        SetPValueCheckIntervalSeconds(3);
     }
 }
