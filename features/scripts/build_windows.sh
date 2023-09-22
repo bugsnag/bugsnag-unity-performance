dea3a0f75b947867afe47f29dd8a0d7070dba847
Richard Elms <richardelms@gmail.com>
2023-09-22 16:00:05 +0200

    windows script conversion

--- features/scripts/build_windows.sh
+++ features/scripts/build_windows.sh
@@ -1,31 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash -e
 
-if [ -z "$UNITY_PERFORMANCE_VERSION" ]
-then
-  echo "UNITY_PERFORMANCE_VERSION must be set"
-  exit 1
-fi
+set -m UNITY_PATH="/mnt/c/Program Files/Unity/Hub/Editor/$UNITY_PERFORMANCE_VERSION/Editor/Unity.exe"
 
-UNITY_PATH="/mnt/c/PROGRA~1/Unity/Hub/Editor/$UNITY_PERFORMANCE_VERSION/Editor/Unity.exe"
-
-
-pushd "${0%/*}"
-  script_path=`pwd`
-popd
-
-pushd "$script_path/../fixtures"
-
-# Run unity and immediately exit afterwards, log all output
 DEFAULT_CLI_ARGS="-quit -batchmode -nographics -logFile build_windows.log"
 
-project_path=`pwd`/mazerunner
+project_path=`pwd`/features/fixtures/mazerunner
 
-# Build for Android
+echo $project_path
 
 $UNITY_PATH $DEFAULT_CLI_ARGS -projectPath $project_path -executeMethod Builder.Windows
 RESULT=$?
 if [ $RESULT -ne 0 ]; then exit $RESULT; fi
-
+  
 mv $project_path/mazerunner_windows $project_path/mazerunner_windows_$UNITY_PERFORMANCE_VERSION
-
 (cd $project_path && zip -q -r mazerunner_windows_$UNITY_PERFORMANCE_VERSION.zip mazerunner_windows_$UNITY_PERFORMANCE_VERSION)
