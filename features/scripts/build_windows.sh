32cd3c17c9bfb3cbc4ae853b5e78c4986304c970
Steve Kirkland-Walton <steve@bugsnag.com>
2023-09-22 16:27:49 +0100

    Convert path using wslpath

--- features/scripts/build_windows.sh
+++ features/scripts/build_windows.sh
@@ -4,13 +4,22 @@ set -m UNITY_PATH="/mnt/c/Program Files/Unity/Hub/Editor/$UNITY_PERFORMANCE_VERS
 
 DEFAULT_CLI_ARGS="-quit -batchmode -nographics -logFile build_windows.log"
 
-project_path=`pwd`/features/fixtures/mazerunner
+# Determine the project path in Windows style to pass to the Unity exe
+SCRIPT_DIR=$(dirname "$(realpath $0)")
+pushd $SCRIPT_DIR
+  pushd ../..
+    root_path=`pwd`
+  popd
+  pushd ../fixtures
+    project_path="$root_path/features/fixtures/mazerunner"
+    project_path=`wslpath -w "$project_path"`
+    echo "Project path: $project_path"
+  popd
+popd
 
-echo $project_path
-
-$UNITY_PATH $DEFAULT_CLI_ARGS -projectPath $project_path -executeMethod Builder.Windows
+"$UNITY_PATH" $DEFAULT_CLI_ARGS -projectPath $project_path -executeMethod Builder.Windows
 RESULT=$?
 if [ $RESULT -ne 0 ]; then exit $RESULT; fi
-  
+
 mv $project_path/mazerunner_windows $project_path/mazerunner_windows_$UNITY_PERFORMANCE_VERSION
 (cd $project_path && zip -q -r mazerunner_windows_$UNITY_PERFORMANCE_VERSION.zip mazerunner_windows_$UNITY_PERFORMANCE_VERSION)
