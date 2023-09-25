0fcbdb2ba6b3ed398713e737d1fd8c4700b0d83c
Richard Elms <richardelms@gmail.com>
2023-09-25 09:31:27 +0200

    typo fix

--- features/scripts/run-windows-ci-tests.sh
+++ features/scripts/run-windows-ci-tests.sh
@@ -4,4 +4,4 @@ pushd features/fixtures/mazerunner/build
 popd
 
 bundle install
-bundle exec maze-runner --app=features/fixtures/maze_runner/build/Windows/mazerunner_windows.exe --os=windows features
+bundle exec maze-runner --app=features/fixtures/mazerunner/build/Windows/mazerunner_windows.exe --os=windows features
