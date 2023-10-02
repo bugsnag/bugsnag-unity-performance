3756b4c9d2fc74c6f5c22ee0f5b2d4f42e89ab65
Richard Elms <richardelms@gmail.com>
2023-10-02 13:39:08 +0200

    typo

--- features/scripts/build_windows.sh
+++ features/scripts/build_windows.sh
@@ -19,8 +19,8 @@ pushd $SCRIPT_DIR
 popd
 
 #remove ios bitcode editor tool as it causes compilation errors
-rm "$win_project_path/Assets/Editor/DisablingBitcodeiOS.cs"
-rm "$win_project_path/Assets/Editor/DisablingBitcodeiOS.cs.meta"
+#rm "$win_project_path/Assets/Editor/DisablingBitcodeiOS.cs"
+#rm "$win_project_path/Assets/Editor/DisablingBitcodeiOS.cs.meta"
 
 UNITY_PATH="/mnt/c/Program Files/Unity/Hub/Editor/$UNITY_PERFORMANCE_VERSION/Editor/Unity.exe"
 "$UNITY_PATH" $DEFAULT_CLI_ARGS \
