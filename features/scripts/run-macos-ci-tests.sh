731b4ab313766c3ffaf8ad6b7409a8488649fc2f
Steve Kirkland-Walton <steve@bugsnag.com>
2023-11-01 14:29:36 +0000

    Use the latest version of Maze Runner for macOS tests

--- features/scripts/run-macos-ci-tests.sh
+++ features/scripts/run-macos-ci-tests.sh
@@ -3,5 +3,6 @@ pushd features/fixtures/mazerunner/
   unzip mazerunner_macos_${UNITY_PERFORMANCE_VERSION:0:4}.zip
 popd
 
+rm -rf Gemfile.lock
 bundle install
 bundle exec maze-runner --app=features/fixtures/mazerunner/mazerunner_macos_${UNITY_PERFORMANCE_VERSION:0:4}.app --os=macos features 
