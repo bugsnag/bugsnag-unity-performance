32e8ee7cacb0c5b242512649fe162319b120bbe0
Richard Elms <richardelms@gmail.com>
2024-06-17 14:42:54 +0200

    typo

--- features/scripts/run-macos-ci-tests.sh
+++ features/scripts/run-macos-ci-tests.sh
@@ -5,4 +5,4 @@ popd
 
 rm -rf Gemfile.lock
 bundle install
-bundle exec maze-runner --app=features/fixtures/mazerunner/mazerunner_macos_dev_${UNITY_PERFORMANCE_VERSION:0:4}.app --os=macos --fail-fast features
+bundle exec maze-runner --app=features/fixtures/mazerunner/mazerunner_macos_${UNITY_PERFORMANCE_VERSION:0:4}.app --os=macos --fail-fast features
