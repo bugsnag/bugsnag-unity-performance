897e7bdcf7cef0db049600cd84d3f4af303ff670
Richard Elms <richardelms@gmail.com>
2024-06-17 14:50:50 +0200

    typo

--- features/scripts/run-macos-ci-tests-dev.sh
+++ features/scripts/run-macos-ci-tests-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 pushd features/fixtures/mazerunner/
-  unzip mazerunner_macos_${UNITY_PERFORMANCE_VERSION:0:4}.zip
+  unzip mazerunner_macos_dev_${UNITY_PERFORMANCE_VERSION:0:4}.zip
 popd
 
 rm -rf Gemfile.lock
