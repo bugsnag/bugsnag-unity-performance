f24eabc61b4092385b10169b38bb2e19bd04c0cf
Richard Elms <richardelms@gmail.com>
2024-07-08 10:20:01 +0200

    e2e tests

---
+++ features/fixtures/mazerunner/Assets/Scripts/Scenarios/Callbacks/CallbackSmokeTests.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BugsnagUnityPerformance;
+using System;
+
+public class CallbackSmokeTests : Scenario
+{
+    public override void PreparePerformanceConfig(string apiKey, string host)
+    {
+        base.PreparePerformanceConfig(apiKey, host);
+        SetMaxBatchSize(1);
+        Configuration.AddOnSpanEndCallback(MyConfigCallback);
+    }
+
+    private bool MyConfigCallback(Span span)
+    {
+        if(span.Name == "discard-me")
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public override void Run()
+    {
+        base.Run();
+        BugsnagPerformance.StartSpan("discard-me").End();
+        BugsnagPerformance.StartSpan("report-me").End();
+    }
+
+}
+
