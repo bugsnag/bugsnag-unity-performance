c5e411be49bd2757b94eed2fb4b0ccf9edb43f27
Richard Elms <richardelms@gmail.com>
2024-09-23 17:36:48 +0200

    windows flag

--- features/scripts/import_package.sh
+++ features/scripts/import_package.sh
@@ -9,12 +9,22 @@ if [ -z "$UNITY_PERFORMANCE_VERSION" ]; then
   exit 1
 fi
 
-if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "mingw"* ]]; then
+# Parse arguments
+BUILD_WINDOWS=false
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    --windows) BUILD_WINDOWS=true ;;
+    *) echo "Unknown option: $1" ;;
+  esac
+  shift
+done
+
+# Set Unity path based on the platform
+if [ "$BUILD_WINDOWS" = true ]; then
   UNITY_PATH="/mnt/c/Program Files/Unity/Hub/Editor/$UNITY_PERFORMANCE_VERSION/Editor/Unity.exe"
 else
   UNITY_PATH="/Applications/Unity/Hub/Editor/$UNITY_PERFORMANCE_VERSION/Unity.app/Contents/MacOS/Unity"
 fi
-
 
 DEFAULT_CLI_ARGS="-batchmode -nographics -quit"
 BUGSNAG_RELEASE_URL="https://github.com/bugsnag/bugsnag-unity/releases/latest/download/Bugsnag.unitypackage"
@@ -31,17 +41,17 @@ if [ $RESULT -ne 0 ]; then
 fi
 
 # Check if Unity path exists
-if [ ! -f "$UNITY_PATH/Unity" ]; then
+if [ ! -f "$UNITY_PATH" ]; then
   echo "Unity executable not found at $UNITY_PATH"
   exit 1
 fi
 
 # Importing the Bugsnag package into Unity project
 echo "Importing Bugsnag.unitypackage into $FIXTURE_PATH"
-$UNITY_PATH/Unity $DEFAULT_CLI_ARGS \
-                  -projectPath $FIXTURE_PATH \
-                  -ignoreCompilerErrors \
-                  -importPackage "$TEMP_DIR/$PACKAGE_NAME"
+$UNITY_PATH $DEFAULT_CLI_ARGS \
+            -projectPath $FIXTURE_PATH \
+            -ignoreCompilerErrors \
+            -importPackage "$TEMP_DIR/$PACKAGE_NAME"
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
   echo "Failed to import Bugsnag.unitypackage"
@@ -55,7 +65,7 @@ rm -rf "$TEMP_DIR"
 echo "Bugsnag package imported successfully"
 
 # Proceed with unzipping the main package
-root_path=`pwd`
+root_path=$(pwd)
 destination="features/fixtures/mazerunner/Packages"
 package="$root_path/upm-package.zip"
 
