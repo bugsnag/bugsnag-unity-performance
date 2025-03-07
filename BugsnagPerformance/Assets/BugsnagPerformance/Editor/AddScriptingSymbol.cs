b40a5c6ae219e20a60f0304cf932a13a59ed0c7f
richard elms <richardelms@gmail.com>
2025-03-07 13:29:49 +0100

    changelog and formatting

--- BugsnagPerformance/Assets/BugsnagPerformance/Editor/AddScriptingSymbol.cs
+++ BugsnagPerformance/Assets/BugsnagPerformance/Editor/AddScriptingSymbol.cs
@@ -10,32 +10,32 @@ public class AddScriptingSymbol : MonoBehaviour
 
     private static BuildTargetGroup[] _supportedPlatforms = { BuildTargetGroup.Android, BuildTargetGroup.Standalone, BuildTargetGroup.iOS, BuildTargetGroup.WebGL };
 
-        static AddScriptingSymbol()
+    static AddScriptingSymbol()
+    {
+        foreach (var target in _supportedPlatforms)
         {
-            foreach (var target in _supportedPlatforms)
+            try
             {
-                try
-                {
-                    SetScriptingSymbol(target);
-                }
-                catch
-                {
-                    // Some users might not have a platform installed, in that case ignore the error
-                }
+                SetScriptingSymbol(target);
+            }
+            catch
+            {
+                // Some users might not have a platform installed, in that case ignore the error
             }
         }
+    }
 
-        static void SetScriptingSymbol(BuildTargetGroup buildTargetGroup)
+    static void SetScriptingSymbol(BuildTargetGroup buildTargetGroup)
+    {
+        var existingSymbols = BugsnagPlayerSettingsCompat.GetScriptingDefineSymbols(buildTargetGroup);
+        if (string.IsNullOrEmpty(existingSymbols))
         {
-            var existingSymbols = BugsnagPlayerSettingsCompat.GetScriptingDefineSymbols(buildTargetGroup);
-            if (string.IsNullOrEmpty(existingSymbols))
-            {
-                existingSymbols = DEFINE_SYMBOL;
-            }
-            else if (!existingSymbols.Contains(DEFINE_SYMBOL))
-            {
-                existingSymbols += ";" + DEFINE_SYMBOL;
-            }
-            BugsnagPlayerSettingsCompat.SetScriptingDefineSymbols(buildTargetGroup, existingSymbols);
+            existingSymbols = DEFINE_SYMBOL;
         }
+        else if (!existingSymbols.Contains(DEFINE_SYMBOL))
+        {
+            existingSymbols += ";" + DEFINE_SYMBOL;
+        }
+        BugsnagPlayerSettingsCompat.SetScriptingDefineSymbols(buildTargetGroup, existingSymbols);
+    }
 }
