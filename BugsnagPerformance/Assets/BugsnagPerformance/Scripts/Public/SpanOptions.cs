b4e820281cff34d232b71c3376c58bd21475d887
richard elms <richardelms@gmail.com>
2025-03-31 12:38:14 +0200

    convert to span metrics object

--- BugsnagPerformance/Assets/BugsnagPerformance/Scripts/Public/SpanOptions.cs
+++ BugsnagPerformance/Assets/BugsnagPerformance/Scripts/Public/SpanOptions.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 namespace BugsnagUnityPerformance
 {
+    public class SpanMetrics
+    {
+        public bool Rendering = false;
+        public bool CPU = false;
+        public bool Memory = false;
+    }
+
     public class SpanOptions
     {
         public DateTimeOffset StartTime = DateTimeOffset.UtcNow;
         public ISpanContext ParentContext = null;
         public bool MakeCurrentContext = true;
         public bool? IsFirstClass = null;
-        public bool? InstrumentRendering = null;
+        public SpanMetrics SpanMetrics = null;
     }
 }
