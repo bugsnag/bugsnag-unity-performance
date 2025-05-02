5800374b8d25076f77bd25eefaf85e1f5d95cf6b
richard elms <richardelms@gmail.com>
2025-05-02 15:24:58 +0200

    typo

--- BugsnagPerformance/Assets/BugsnagPerformance/Scripts/Native/iOSNative.cs
+++ BugsnagPerformance/Assets/BugsnagPerformance/Scripts/Native/iOSNative.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-#if UNITY_IOS //&& !UNITY_EDITOR
+#if UNITY_IOS && !UNITY_EDITOR
 using System.Runtime.InteropServices;
 #endif
 using UnityEngine;
