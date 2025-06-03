b602448d98bf9a96507805d32b5da780af9a32f8
richard elms <richardelms@gmail.com>
2025-06-03 14:05:00 +0200

    trigger full build on release branches

--- .buildkite/pipeline_trigger.sh
+++ .buildkite/pipeline_trigger.sh
@@ -3,6 +3,7 @@
 if [[ "$BUILDKITE_MESSAGE" == *"[full ci]"* ||
   "$BUILDKITE_BRANCH" == "next" ||
   "$BUILDKITE_BRANCH" == "main" ||
+  "$BUILDKITE_BRANCH" == releases/* ||
   ! -z "$FULL_SCHEDULED_BUILD" ||
   "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "main" ]]; then
   echo "Running full build"
