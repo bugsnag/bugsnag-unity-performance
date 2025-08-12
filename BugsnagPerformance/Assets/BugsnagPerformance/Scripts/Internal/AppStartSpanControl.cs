dd341c9c28310bf4715c3431fcbc575afb81ed33
richard elms <richardelms@gmail.com>
2025-08-12 13:49:52 +0200

    thread safe refactor

--- BugsnagPerformance/Assets/BugsnagPerformance/Scripts/Internal/AppStartSpanControl.cs
+++ BugsnagPerformance/Assets/BugsnagPerformance/Scripts/Internal/AppStartSpanControl.cs
@@ -3,30 +3,18 @@ namespace BugsnagUnityPerformance
     internal class AppStartSpanControl : IAppStartSpanControl
     {
         private const string APP_START_SPAN_PREFIX = "[AppStart/UnityRuntime]";
-        private const string APP_START_NAME_ATTRIBUTE = "bugsnag.app_start.name";
         private readonly Span _span;
 
-        public AppStartSpanControl(Span span)
-        {
-            _span = span;
-        }
+        public AppStartSpanControl(Span span) => _span = span;
 
         public void SetType(string type)
         {
-            if (_span != null && !_span.Ended)
-            {
-                _span.Name = APP_START_SPAN_PREFIX + type;
-                _span.SetAttributeInternal(APP_START_NAME_ATTRIBUTE, type);
-            }
+            _span?.TrySetAppStartType(type, APP_START_SPAN_PREFIX);
         }
 
         public void ClearType()
         {
-            if (_span != null && !_span.Ended)
-            {
-                _span.Name = APP_START_SPAN_PREFIX;
-                _span.SetAttributeInternal(APP_START_NAME_ATTRIBUTE, (string)null);
-            }
+            _span?.TrySetAppStartType(null, APP_START_SPAN_PREFIX);
         }
     }
 }
