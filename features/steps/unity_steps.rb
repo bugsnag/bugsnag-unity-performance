800d2299f460e9b05c63159c9966eda6cfbf872f
Steve Kirkland <twometresteve@users.noreply.github.com>
2025-10-22 09:58:40 +0100

    Use Maze Runner idempotent commands in e2e tests (#209)
    * Use Maze Runner idemopotent commands
    
    * Update Cucumber steps for idempotent commands
    
    ---------
    
    Co-authored-by: Steve Kirkland <steve.kirkland@smartbear.com>

--- features/steps/unity_steps.rb
+++ features/steps/unity_steps.rb
@@ -6,11 +6,6 @@ def execute_command(action, scenario_name = '')
     scenarioName: scenario_name
   }
   Maze::Server.commands.add command
-
-  # Ensure fixture has read the command
-  count = 300
-  sleep 0.1 until Maze::Server.commands.remaining.empty? || (count -= 1) < 1
-  raise 'Test fixture did not GET /command' unless Maze::Server.commands.remaining.empty?
 end
 
 When('I clear the Bugsnag cache') do
@@ -30,36 +25,14 @@ end
 When('I run the game in the {string} state') do |state|
   platform = Maze::Helper.get_current_platform
   case platform
-  when 'macos'
-    # Call executable directly rather than use open, which flakes on CI
-    log = File.join(Dir.pwd, "#{state}-mazerunner.log")
-    command = "#{Maze.config.app}/Contents/MacOS/Mazerunner --args -logfile #{log} > /dev/null"
-    Maze::Runner.run_command(command, blocking: false)
-
+  when 'macos','android', 'ios', 'switch', 'windows'
     execute_command('run_scenario', state)
-
-  when 'windows'
-    win_log = File.join(Dir.pwd, "#{state}-mazerunner.log")
-    command = "#{Maze.config.app} --args -logfile #{win_log}"
-    Maze::Runner.run_command(command, blocking: false)
-
-    execute_command('run_scenario', state)
-
-  when 'android', 'ios'
-    execute_command('run_scenario', state)
-
   when 'browser'
     # WebGL in a browser
     url = "http://localhost:#{Maze.config.port}/docs/index.html"
     $logger.debug "Navigating to URL: #{url}"
     step("I navigate to the URL \"#{url}\"")
     execute_command('run_scenario', state)
-
-  when 'switch'
-
-    switch_run_on_target
-    execute_command('run_scenario', state)
-
   else
     raise "Platform #{platform} has not been considered"
   end
